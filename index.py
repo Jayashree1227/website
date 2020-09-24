d78061a6d361ea1a4e01ebd7f4a455efdbe52e39
Jayashree1227 <jayashreepselvammba@gmail.com>
2020-09-24 15:13:53 +0530

    "Beginning 1st change - variable and Data types "

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
-import pandas as pd 
+# Day 1 # 66 days of data # |||| Starts with basic - DATAQuest.io
+                       # Variables and Data Types
 
+import pandas as pd
 a = 1
 b = 2
 
 c = (a + b)
 
 print(c)
+
+
+
+
+
+
+
+
