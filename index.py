09618336b31e9b61c867de7af07b3071fbd9770b
Jayashree1227 <jayashreepselvammba@gmail.com>
2020-09-25 14:49:50 +0530

    LIST - Data Type

--- index.py
+++ index.py
@@ -53,4 +53,23 @@ print (str + "TEST") # Prints concatenated string
 
 
 
+# Python Lists
+
+list = [ 'abcd' , 786 , 2.23, 'john', 70.2 ]
+print(list)
+
+tinylist = [123, 'john']
+print(tinylist)
+
+# SLICING 
+print (list)          # Prints complete list
+print (list[0])       # Prints first element of the list
+print (list[1:3])     # Prints elements starting from 2nd till 3rd 
+print (list[2:])      # Prints elements starting from 3rd element
+print (tinylist * 2)  # Prints list two times
+print (list + tinylist) # Prints concatenated lists
+
+
+
+
 
